7c52fc51af5db3c02e6c66c36b8097414d3e9b7d
Helen <helen.youchen@gmail.com>
2024-01-04 02:02:09 +0800

    加入專案檔案。

---
+++ prjSignalRDemo/Hubs/ChatHub.cs
@@ -0,0 +1,85 @@
+﻿
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+
+namespace prjSignalRDemo.Hubs
+{
+    public class ChatHub :Hub
+    {
+        //使用 : Hub 這部分表示 ChatHub 類別是 Hub 類別的衍生（繼承）類別。這種繼承的概念意味著 ChatHub 類別將繼承 Hub 類別的屬性和方法。這個類別是 SignalR 框架提供的 Hub 類別的衍生類別，用於實現 SignalR 的擴充功能
+        
+        // 用戶連線 ID 列表
+        public static List<string> ConnIDList = new List<string>();
+
+        /// <summary>
+        /// 連線事件
+        /// </summary>
+        /// <returns></returns>
+        public override async Task OnConnectedAsync()
+        {
+
+            if (ConnIDList.Where(p => p == Context.ConnectionId).FirstOrDefault() == null)
+            {
+                ConnIDList.Add(Context.ConnectionId);
+            }
+            // 更新連線 ID 列表
+            string jsonString = JsonConvert.SerializeObject(ConnIDList);
+            await Clients.All.SendAsync("UpdList", jsonString);
+
+            // 更新個人 ID
+            await Clients.Client(Context.ConnectionId).SendAsync("UpdSelfID", Context.ConnectionId);
+
+            // 更新聊天內容
+            await Clients.All.SendAsync("UpdContent", "新連線 ID: " + Context.ConnectionId);
+
+            await base.OnConnectedAsync();
+        }
+
+        /// <summary>
+        /// 離線事件
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            string id = ConnIDList.Where(p => p == Context.ConnectionId).FirstOrDefault();
+            if (id != null)
+            {
+                ConnIDList.Remove(id);
+            }
+            // 更新連線 ID 列表
+            string jsonString = JsonConvert.SerializeObject(ConnIDList);
+            await Clients.All.SendAsync("UpdList", jsonString);
+
+            // 更新聊天內容
+            await Clients.All.SendAsync("UpdContent", "已離線 ID: " + Context.ConnectionId);
+
+            await base.OnDisconnectedAsync(ex);
+        }
+
+        /// <summary>
+        /// 傳遞訊息
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="message"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task SendMessage(string selfID, string message, string sendToID)
+        {
+            if (string.IsNullOrEmpty(sendToID))
+            {
+                await Clients.All.SendAsync("UpdContent", selfID + " 說: " + message);
+            }
+            else
+            {
+                // 接收人
+                await Clients.Client(sendToID).SendAsync("UpdContent", selfID + " 私訊向你說: " + message);
+
+                // 發送人
+                await Clients.Client(Context.ConnectionId).SendAsync("UpdContent", "你向 " + sendToID + " 私訊說: " + message);
+            }
+        }
+
+
+    }
+}
